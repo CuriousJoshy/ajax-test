19094f50e3176fc7a79b7eb2c7dc59ac85c13f90
CuriousJoshy <47984839+CuriousJoshy@users.noreply.github.com>
2019-03-15 12:21:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+function append(text)
+{
+    var p = document.createElement("p");
+    p.innerText = text;
+
+    document.body.appendChild(p);
+}
+
 var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
+    append(this.readyState + " " + this.satus);
+
     if (this.readyState == 4 && this.status == 200) {
         document.write("Response: " + this.responseText);
     }
